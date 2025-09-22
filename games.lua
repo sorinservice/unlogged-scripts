6737ead9c1372e66409c04d91395dc176546c1a1
sorinservice <nemesisofnocturn@gmail.com>
2025-09-22 16:00:45 +0200

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,6 @@
+-- games.lua
+return {
+    {Name = "Emergency Hamburg", PlaceId = 7711635737},
+    {Name = "99 Nights in the Forest", PlaceId = 79546208627805},
+    {Name = "RealisticCarDriving.lua", PlaceId = 5829141886},
+}
