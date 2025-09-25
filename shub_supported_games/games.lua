3fe489f2c0af3525a1474f8d826a49d3356e7f38
sorinservice <nemesisofnocturn@gmail.com>
2025-09-25 04:00:39 +0200

    Update and rename games.lua to shub_supported_games/games.lua

---
+++ shub_supported_games/games.lua
@@ -0,0 +1,7 @@
+--unlogged-scripts/shub_supported_games/games.lua
+return {
+    {Name = "Emergency Hamburg", ScriptCount = 3},
+    {Name = "99 Nights in the Forest", ScriptCount = 2},
+    {Name = "RealisticCarDriving", ScriptCount = 1},
+    {Name = "Break your Bones", ScriptCount = 1}
+}
