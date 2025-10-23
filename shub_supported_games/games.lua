32679f1a47fe9fc51d843baf31af5a4071f62827
EndOfCircuit <nemesisofnocturn@gmail.com>
2025-10-23 07:02:15 +0200

    Update games.lua

--- shub_supported_games/games.lua
+++ shub_supported_games/games.lua
@@ -1,7 +1,21 @@
 --unlogged-scripts/shub_supported_games/games.lua
-return {
+local games = {
+    {Name = "3008 [2.73]", ScriptCount = 2},
     {Name = "Emergency Hamburg", ScriptCount = 3},
     {Name = "99 Nights in the Forest", ScriptCount = 2},
     {Name = "RealisticCarDriving", ScriptCount = 1},
-    {Name = "Break your Bones", ScriptCount = 1}
+    {Name = "Break your Bones", ScriptCount = 1},
 }
+
+
+
+for _, game in ipairs(games) do
+    local scriptCount = tonumber(game.ScriptCount)
+    if not scriptCount then
+        scriptCount = 0
+    end
+    game.ScriptCount = scriptCount
+end
+
+return games
+
